42818c3a332af835ea5484f0eceb2c31c7cdb0a2
Sneha Baskaran <sneha.baskaran@itp.objectfrontier.com>
2023-09-07 23:12:08 +0530

    HI

--- Program.cs
+++ Program.cs
@@ -5,3 +5,26 @@ interface IBaseclass{
     public void ShowDeposit(); 
 
 }
+
+class Derivedclass : IBaseclass
+{
+    public void ShowDeposit()
+    {
+        Console.WriteLine("The Deposited amount is 3000");
+    }
+
+    public void ShowInterest()
+    {
+        Console.WriteLine("2% interest");
+    }
+}
+
+class Program{
+     static void Main(string[] args){
+        Derivedclass MyClass = new Derivedclass();
+        MyClass.ShowDeposit();         
+        MyClass.ShowInterest();         
+
+     }
+
+}
